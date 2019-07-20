a0fb0e045ea19736b8f4a09ab3db56182b3cf337
Travis <travisyjcao@gmail.com>
2019-07-20 11:17:41 +0800

    individual version

--- linkBuildPredict.py
+++ linkBuildPredict.py
@@ -1,70 +1,67 @@
 import twint
 import networkx as nx
 
-
-
 # f = open('testUsers.txt', 'r')
 f = open('predictUser.txt', 'r')
 
+# get the user graph from the txt
 userStr = f.read()
-
 userDict = eval(userStr)
-
 userGraph = nx.DiGraph(userDict)
 
 print("Nodes number: %d" % (userGraph.number_of_nodes()))
 print("Edges number: %d" % (userGraph.number_of_edges()))
 
-userList = set(userGraph.nodes)
+userList = list(userGraph.nodes)
+userList.sort()
+
+userList1 = [userList[i] for i in range(0, 100)]
 
 count = 0
-for user in userList:
+for user in userList1:
     c = twint.Config()
     c.Username = user
     c.Store_object = True
-    c.Limit = 100
-    print("user: %s 's follower" % (user))
-    twint.run.Followers(c)
+    print("user %s 's followings:" % (user))
+    twint.run.Following(c)
 
-    count += 100
+    followings = twint.output.follows_list
 
-    followers = twint.output.follows_list
-    followers = set(followers)
-    overlap = followers & userList
-    for i in overlap:
+    intersection = [user for user in userList1 if user in followings]
+    for i in intersection:
         userGraph.add_edge(i, user)
 
+    # clear the list
     twint.output.follows_list = []
 
-    if count == 1000:
-        output = nx.to_dict_of_dicts(userGraph)
-        strOutput = str(output)
-        f = open('PredictUserDONE.txt', 'w')
-        f.write(strOutput)
-        f.close()
-        print("File saved")
-        print("count : %d" %(count))
+    output = nx.to_dict_of_dicts(userGraph)
+    strOutput = str(output)
+    f = open('testUserDONE.txt', 'w')
+    f.write(strOutput)
+    f.close()
 
-    if count % 1000 == 0 and count > 1000:
-        output = nx.to_dict_of_dicts(userGraph)
-        strOutput = str(output)
-        f = open('PredictUserDONE.txt', 'w')
-        f.write(strOutput)
-        f.close()
-        print("File saved")
-        print("count : %d" %(count))
-    #         break
-    # break
+    edgeListOutput = str(list(userGraph.edges))
+    edgeListFile = open('userEdgeList.txt', 'w')
+    edgeListFile.write(edgeListOutput)
+    edgeListFile.close()
 
-print("add Complete")
+    print("File saved")
 
-output = nx.to_dict_of_dicts(userGraph)
-strOutput = str(output)
-f = open('PredictUserDONE.txt', 'w')
-f.write(strOutput)
-f.close()
+print("ADD Complete")
 
-print("save complete")
+# # save the graph to txt
+# output = nx.to_dict_of_dicts(userGraph)
+# strOutput = str(output)
+# f = open('testUserDONE.txt', 'w')
+# f.write(strOutput)
+# f.close()
 
-print(list(userGraph.edges))
+# edgeListOutput = str(list(userGraph.edges))
+# edgeListFile = open('userEdgeList.txt', 'w')
+# edgeListFile.write(edgeListOutput)
+# edgeListFile.close()
+
+print("SAVE complete")
+
+
 
