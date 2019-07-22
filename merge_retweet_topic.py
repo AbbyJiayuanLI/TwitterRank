ed1bacffadf4615e72792b32079ba09810aa8be4
Travis <travisyjcao@gmail.com>
2019-07-23 01:42:51 +0800

    merge each movie csv

--- merge_retweet_topic.py
+++ merge_retweet_topic.py
@@ -1,188 +1,134 @@
-f2 = eval(open('./yanxiao/user_retweet_TEST1.txt', 'r').read())
-f3 = eval(open('./yanxiao/user_retweet_TEST2.txt', 'r').read())
-f4 = eval(open('./yanxiao/user_retweet_TEST3.txt', 'r').read())
-f5 = eval(open('./yanxiao/user_retweet_TEST4.txt', 'r').read())
-f6 = eval(open('./yanxiao/user_retweet_TEST5.txt', 'r').read())
-f7 = eval(open('./yanxiao/user_retweet_TEST6.txt', 'r').read())
-f8 = eval(open('./yanxiao/user_retweet_TEST7.txt', 'r').read())
-f9 = eval(open('./yanxiao/user_retweet_TEST8.txt', 'r').read())
-f10 = eval(open('./yanxiao/user_retweet_TEST9.txt', 'r').read())
-f11 = eval(open('./yanxiao/user_retweet_TEST10.txt', 'r').read())
-f12 = eval(open('./yanxiao/user_retweet_TEST11.txt', 'r').read())
-f13 = eval(open('./yanxiao/user_retweet_TEST12.txt', 'r').read())
-f14 = eval(open('./Abby/user_retweet_TEST1.txt','r').read())
-f15 = eval(open('./Abby/user_retweet_TEST2.txt','r').read())
-f16 = eval(open('./Abby/user_retweet_TEST3.txt','r').read())
-f17 = eval(open('./Abby/user_retweet_TEST4.txt','r').read())
-f18 = eval(open('./Abby/user_retweet_TEST5.txt','r').read())
-f19 = eval(open('./Abby/user_retweet_TEST6.txt','r').read())
-f20 = eval(open('./Abby/user_retweet_TEST7.txt','r').read())
-f21 = eval(open('./Abby/user_retweet_TEST8.txt','r').read())
-f22 = eval(open('./Abby/user_retweet_TEST9.txt','r').read())
-f23 = eval(open('./Abby/user_retweet_TEST10.txt','r').read())
-f24 = eval(open('./Abby/user_retweet_TEST11.txt','r').read())
+import csv
 
-g2 = eval(open('./yanxiao/user_TopicCount_TEST1.txt', 'r').read())
-g3 = eval(open('./yanxiao/user_TopicCount_TEST2.txt', 'r').read())
-g4 = eval(open('./yanxiao/user_TopicCount_TEST3.txt', 'r').read())
-g5 = eval(open('./yanxiao/user_TopicCount_TEST4.txt', 'r').read())
-g6 = eval(open('./yanxiao/user_TopicCount_TEST5.txt', 'r').read())
-g7 = eval(open('./yanxiao/user_TopicCount_TEST6.txt', 'r').read())
-g8 = eval(open('./yanxiao/user_TopicCount_TEST7.txt', 'r').read())
-g9 = eval(open('./yanxiao/user_TopicCount_TEST8.txt', 'r').read())
-g10 = eval(open('./yanxiao/user_TopicCount_TEST9.txt', 'r').read())
-g11 = eval(open('./yanxiao/user_TopicCount_TEST10.txt', 'r').read())
-g12 = eval(open('./yanxiao/user_TopicCount_TEST11.txt', 'r').read())
-g13 = eval(open('./yanxiao/user_TopicCount_TEST12.txt', 'r').read())
-g14 = eval(open('./Abby/user_TopicCount_TEST1.txt', 'r').read())
-g15 = eval(open('./Abby/user_TopicCount_TEST2.txt', 'r').read())
-g16 = eval(open('./Abby/user_TopicCount_TEST3.txt', 'r').read())
-g17 = eval(open('./Abby/user_TopicCount_TEST4.txt', 'r').read())
-g18 = eval(open('./Abby/user_TopicCount_TEST5.txt', 'r').read())
-g19 = eval(open('./Abby/user_TopicCount_TEST6.txt', 'r').read())
-g20 = eval(open('./Abby/user_TopicCount_TEST7.txt', 'r').read())
-g21 = eval(open('./Abby/user_TopicCount_TEST8.txt', 'r').read())
-g22 = eval(open('./Abby/user_TopicCount_TEST9.txt', 'r').read())
-g23 = eval(open('./Abby/user_TopicCount_TEST10.txt', 'r').read())
-g24 = eval(open('./Abby/user_TopicCount_TEST11.txt', 'r').read())
+f1 = eval(open('./yanxiao/each_movie_TEST1.txt', 'r').read())
+f2 = eval(open('./yanxiao/each_movie_TEST2.txt', 'r').read())
+f3 = eval(open('./yanxiao/each_movie_TEST3.txt', 'r').read())
+f4 = eval(open('./yanxiao/each_movie_TEST4.txt', 'r').read())
+f5 = eval(open('./yanxiao/each_movie_TEST5.txt', 'r').read())
+f6 = eval(open('./yanxiao/each_movie_TEST6.txt', 'r').read())
+f7 = eval(open('./travis/each_movie_TEST1.txt', 'r').read())
+f8 = eval(open('./travis/each_movie_TEST2.txt', 'r').read())
+f9 = eval(open('./travis/each_movie_TEST3.txt', 'r').read())
+f10 = eval(open('./travis/each_movie_TEST4.txt', 'r').read())
+f11 = eval(open('./travis/each_movie_TEST5.txt', 'r').read())
+f12 = eval(open('./travis/each_movie_TEST6.txt', 'r').read())
+f13 = eval(open('./travis/each_movie_TEST7.txt', 'r').read())
+f14 = eval(open('./travis/each_movie_TEST8.txt', 'r').read())
 
-m1 = eval(open('./yanxiao/user_retweet_PREDICT1.txt', 'r').read())
-m2 = eval(open('./yanxiao/user_retweet_PREDICT2.txt', 'r').read())
-m3 = eval(open('./yanxiao/user_retweet_PREDICT3.txt', 'r').read())
-m4 = eval(open('./yanxiao/user_retweet_PREDICT4.txt', 'r').read())
-m5 = eval(open('./yanxiao/user_retweet_PREDICT5.txt', 'r').read())
-m6 = eval(open('./yanxiao/user_retweet_PREDICT6.txt', 'r').read())
-m7 = eval(open('./yanxiao/user_retweet_PREDICT7.txt', 'r').read())
-m8 = eval(open('./yanxiao/user_retweet_PREDICT8.txt', 'r').read())
-m9 = eval(open('./yanxiao/user_retweet_PREDICT9.txt', 'r').read())
-m10 = eval(open('./yanxiao/user_retweet_PREDICT10.txt', 'r').read())
-m11 = eval(open('./yanxiao/user_retweet_PREDICT12.txt', 'r').read())
-m12 = eval(open('./yanxiao/user_retweet_PREDICT11.txt', 'r').read())
-m13 = eval(open('./travis/user_retweet_PREDICT1.txt', 'r').read())
-m14 = eval(open('./travis/user_retweet_PREDICT2.txt', 'r').read())
-m15 = eval(open('./travis/user_retweet_PREDICT3.txt', 'r').read())
-m16 = eval(open('./travis/user_retweet_PREDICT4.txt', 'r').read())
-m17 = eval(open('./travis/user_retweet_PREDICT5.txt', 'r').read())
-m18 = eval(open('./travis/user_retweet_PREDICT6.txt', 'r').read())
-m19 = eval(open('./travis/user_retweet_PREDICT8.txt', 'r').read())
-m20 = eval(open('./travis/user_retweet_PREDICT9.txt', 'r').read())
-m21 = eval(open('./travis/user_retweet_PREDICT7.txt', 'r').read())
-
-n1 = eval(open('./yanxiao/user_TopicCount_PREDICT1.txt', 'r').read())
-n2 = eval(open('./yanxiao/user_TopicCount_PREDICT2.txt', 'r').read())
-n3 = eval(open('./yanxiao/user_TopicCount_PREDICT3.txt', 'r').read())
-n4 = eval(open('./yanxiao/user_TopicCount_PREDICT4.txt', 'r').read())
-n5 = eval(open('./yanxiao/user_TopicCount_PREDICT5.txt', 'r').read())
-n6 = eval(open('./yanxiao/user_TopicCount_PREDICT6.txt', 'r').read())
-n7 = eval(open('./yanxiao/user_TopicCount_PREDICT7.txt', 'r').read())
-n8 = eval(open('./yanxiao/user_TopicCount_PREDICT8.txt', 'r').read())
-n9 = eval(open('./yanxiao/user_TopicCount_PREDICT9.txt', 'r').read())
-n10 = eval(open('./yanxiao/user_TopicCount_PREDICT10.txt', 'r').read())
-n11 = eval(open('./yanxiao/user_TopicCount_PREDICT11.txt', 'r').read())
-n12 = eval(open('./yanxiao/user_TopicCount_PREDICT12.txt', 'r').read())
-n13 = eval(open('./travis/user_TopicCount_PREDICT1.txt', 'r').read())
-n14 = eval(open('./travis/user_TopicCount_PREDICT2.txt', 'r').read())
-n15 = eval(open('./travis/user_TopicCount_PREDICT3.txt', 'r').read())
-n16 = eval(open('./travis/user_TopicCount_PREDICT4.txt', 'r').read())
-n18 = eval(open('./travis/user_TopicCount_PREDICT5.txt', 'r').read())
-n19 = eval(open('./travis/user_TopicCount_PREDICT6.txt', 'r').read())
-n17 = eval(open('./travis/user_TopicCount_PREDICT7.txt', 'r').read())
-n21 = eval(open('./travis/user_TopicCount_PREDICT9.txt', 'r').read())
-
-q1 = eval(open('./yanxiao/user_Topic_tweet_Count_PREDICT1.txt', 'r').read())
-q2 = eval(open('./yanxiao/user_Topic_tweet_Count_PREDICT2.txt', 'r').read())
-q3 = eval(open('./yanxiao/user_Topic_tweet_Count_PREDICT3.txt', 'r').read())
-q4 = eval(open('./yanxiao/user_Topic_tweet_Count_PREDICT4.txt', 'r').read())
-q5 = eval(open('./yanxiao/user_Topic_tweet_Count_PREDICT5.txt', 'r').read())
-q6 = eval(open('./yanxiao/user_Topic_tweet_Count_PREDICT6.txt', 'r').read())
-q7 = eval(open('./yanxiao/user_Topic_tweet_Count_PREDICT7.txt', 'r').read())
-q8 = eval(open('./yanxiao/user_Topic_tweet_Count_PREDICT8.txt', 'r').read())
-q9 = eval(open('./yanxiao/user_Topic_tweet_Count_PREDICT9.txt', 'r').read())
-q10 = eval(open('./yanxiao/user_Topic_tweet_Count_PREDICT10.txt', 'r').read())
-q11 = eval(open('./yanxiao/user_Topic_tweet_Count_PREDICT11.txt', 'r').read())
-q12 = eval(open('./yanxiao/user_Topic_tweet_Count_PREDICT12.txt', 'r').read())
-q13 = eval(open('./travis/user_Topic_tweet_Count_PREDICT1.txt', 'r').read())
-q14 = eval(open('./travis/user_Topic_tweet_Count_PREDICT2.txt', 'r').read())
-q15 = eval(open('./travis/user_Topic_tweet_Count_PREDICT3.txt', 'r').read())
-q16 = eval(open('./travis/user_Topic_tweet_Count_PREDICT4.txt', 'r').read())
-q17 = eval(open('./travis/user_Topic_tweet_Count_PREDICT5.txt', 'r').read())
-q18 = eval(open('./travis/user_Topic_tweet_Count_PREDICT6.txt', 'r').read())
-q19 = eval(open('./travis/user_Topic_tweet_Count_PREDICT7.txt', 'r').read())
-q20 = eval(open('./travis/user_Topic_tweet_Count_PREDICT8.txt', 'r').read())
-q21 = eval(open('./travis/user_Topic_tweet_Count_PREDICT9.txt', 'r').read())
-
-a1 = eval(open('./yanxiao/user_Topic_tweet_Count_TEST1.txt', 'r').read())
-a2 = eval(open('./yanxiao/user_Topic_tweet_Count_TEST2.txt', 'r').read())
-a3 = eval(open('./yanxiao/user_Topic_tweet_Count_TEST3.txt', 'r').read())
-a4 = eval(open('./yanxiao/user_Topic_tweet_Count_TEST4.txt', 'r').read())
-a5 = eval(open('./yanxiao/user_Topic_tweet_Count_TEST5.txt', 'r').read())
-a6 = eval(open('./yanxiao/user_Topic_tweet_Count_TEST6.txt', 'r').read())
-a7 = eval(open('./yanxiao/user_Topic_tweet_Count_TEST7.txt', 'r').read())
-a8 = eval(open('./yanxiao/user_Topic_tweet_Count_TEST8.txt', 'r').read())
-a9 = eval(open('./yanxiao/user_Topic_tweet_Count_TEST9.txt', 'r').read())
-a10 = eval(open('./yanxiao/user_Topic_tweet_Count_TEST10.txt', 'r').read())
-a11 = eval(open('./yanxiao/user_Topic_tweet_Count_TEST11.txt', 'r').read())
-a12 = eval(open('./yanxiao/user_Topic_tweet_Count_TEST12.txt', 'r').read())
-a13 = eval(open('./Abby/user_Topic_tweet_Count_TEST1.txt', 'r').read())
-a14 = eval(open('./Abby/user_Topic_tweet_Count_TEST2.txt', 'r').read())
-a15 = eval(open('./Abby/user_Topic_tweet_Count_TEST3.txt', 'r').read())
-a16 = eval(open('./Abby/user_Topic_tweet_Count_TEST4.txt', 'r').read())
-a17 = eval(open('./Abby/user_Topic_tweet_Count_TEST5.txt', 'r').read())
-a18 = eval(open('./Abby/user_Topic_tweet_Count_TEST6.txt', 'r').read())
-a19 = eval(open('./Abby/user_Topic_tweet_Count_TEST7.txt', 'r').read())
-a20 = eval(open('./Abby/user_Topic_tweet_Count_TEST8.txt', 'r').read())
-a21 = eval(open('./Abby/user_Topic_tweet_Count_TEST9.txt', 'r').read())
-a22 = eval(open('./Abby/user_Topic_tweet_Count_TEST10.txt', 'r').read())
-a23 = eval(open('./Abby/user_Topic_tweet_Count_TEST11.txt', 'r').read())
+n1 = eval(open('./yanxiao/each_movie_PREDICT1.txt', 'r').read())
+n2 = eval(open('./yanxiao/each_movie_PREDICT2.txt', 'r').read())
+n3 = eval(open('./yanxiao/each_movie_PREDICT3.txt', 'r').read())
+n4 = eval(open('./yanxiao/each_movie_PREDICT4.txt', 'r').read())
+n5 = eval(open('./yanxiao/each_movie_PREDICT5.txt', 'r').read())
+n6 = eval(open('./yanxiao/each_movie_PREDICT6.txt', 'r').read())
+n7 = eval(open('./travis/each_movie_PREDICT1.txt', 'r').read())
+n8 = eval(open('./travis/each_movie_PREDICT2.txt', 'r').read())
+n9 = eval(open('./travis/each_movie_PREDICT3.txt', 'r').read())
+n10 = eval(open('./travis/each_movie_PREDICT4.txt', 'r').read())
+n11 = eval(open('./travis/each_movie_PREDICT5.txt', 'r').read())
+n12 = eval(open('./travis/each_movie_PREDICT6.txt', 'r').read())
+n13 = eval(open('./travis/each_movie_PREDICT7.txt', 'r').read())
+n14 = eval(open('./travis/each_movie_PREDICT8.txt', 'r').read())
 
 
-retweet_dict_TEST = {**f2, **f3, **f4, **f5, **f6, **f7, **f8,
-                     **f9, **f10, **f11, **f12, **f13, **f14, **f15,
-                     **f16, **f17, **f18, **f19, **f20, **f21, **f22,
-                     **f23, **f24}
+predict = []
+test = []
 
-topic_number_dict_TEST = {**g2, **g3, **g4, **g5, **g6, **g7, **g8,
-                     **g9, **g10, **g11, **g12, **g13, **g14, **g15,
-                     **g16, **g17, **g18, **g19, **g20, **g21, **g22,
-                     **g23, **g24}
 
-retweet_dict_PREDICT = {**m1, **m2, **m3, **m4, **m5, **m6, **m7, **m8,
-                     **m9, **m10, **m11, **m12, **m13, **m14, **m15,
-                        **m16, **m17, **m18, **m19, **m20, **m21}
+def jjj(l1, l2):
+    for i in l1:
+        l2.append(i)
+    return l2
 
-topic_number_dict_PREDICT = {**n1, **n2, **n3, **n4, **n5, **n6, **n7, **n8,
-                     **n9, **n10, **n11, **n12, **n13, **n14, **n15,
-                     **n16, **n17, **n18, **n19, **n21}
+test = jjj(f1, test)
+test = jjj(f2, test)
+test = jjj(f3, test)
+test = jjj(f4, test)
+test = jjj(f5, test)
+test = jjj(f6, test)
+test = jjj(f7, test)
+test = jjj(f8, test)
+test = jjj(f9, test)
+test = jjj(f10, test)
+test = jjj(f11, test)
+test = jjj(f12, test)
+test = jjj(f13, test)
+test = jjj(f14, test)
 
-topic_tweets_PREDICT = {**q1, **q2, **q3, **q4, **q5, **q6, **q7, **q8,
-                     **q9, **q10, **q11, **q12, **q13, **q14, **q15,
-                        **q16, **q17, **q18, **q19, **q20, **q21}
 
-topic_tweets_TEST = {**a1, **a2, **a3, **a4, **a5, **a6, **a7, **a8,
-                     **a9, **a10, **a11, **a12, **a13, **a14, **a15,
-                        **a16, **a17, **a18, **a19, **a20, **a21,
-                     **a22, **a23}
+predict = jjj(n1, predict)
+print(len(predict))
+predict = jjj(n2, predict)
+print(len(predict))
+predict = jjj(n3, predict)
+print(len(predict))
+predict = jjj(n4, predict)
+predict = jjj(n5, predict)
+print(len(predict))
+predict = jjj(n6, predict)
+print(len(predict))
+predict = jjj(n7, predict)
+print(len(predict))
+predict = jjj(n9, predict)
+print(len(predict))
+predict = jjj(n10, predict)
+print(len(predict))
+predict = jjj(n11, predict)
+print(len(predict))
+predict = jjj(n12, predict)
+print(len(predict))
+predict = jjj(n13, predict)
+print(len(predict))
+predict = jjj(n14, predict)
+print(len(predict))
 
-output_retweet_dict_TEST = open('retweet_dict_TEST.txt', 'w')
-output_retweet_dict_PREDICT = open('retweet_dict_PREDICT.txt', 'w')
-output_topic_number_dict_TEST = open('topic_number_dict_TEST.txt', 'w')
-output_topic_number_dict_PREDICT = open('topic_number_dict_PREDICT.txt', 'w')
-output_topic_tweets_dict_TEST = open('topic_tweets_dict_TEST.txt', 'w')
-output_topic_tweets_dict_PREDICT = open('topic_tweets_dict_PREDICT.txt', 'w')
+# print(predict)
 
-output_retweet_dict_TEST.write(str(retweet_dict_TEST))
-output_retweet_dict_PREDICT.write(str(retweet_dict_PREDICT))
-output_topic_number_dict_TEST.write(str(topic_number_dict_TEST))
-output_topic_number_dict_PREDICT.write(str(topic_number_dict_PREDICT))
-output_topic_tweets_dict_TEST.write(str(topic_tweets_TEST))
-output_topic_tweets_dict_PREDICT.write(str(topic_tweets_PREDICT))
+print(len(n1))
+print(len(n2))
+print(len(n3))
+print(len(n4))
+print(len(n5))
+print(len(n6))
+print(len(n7))
+print(len(n8))
+print(len(n9))
+print(len(n10))
+print(len(n11))
+print(len(n12))
+print(len(n13))
+print(len(n14))
 
-output_retweet_dict_TEST.close()
-output_retweet_dict_PREDICT.close()
-output_topic_number_dict_PREDICT.close()
-output_topic_number_dict_TEST.close()
-output_topic_tweets_dict_TEST.close()
-output_topic_tweets_dict_PREDICT.close()
+print(len(test))
+def export_predict(filename, list1):
+    csv_columns = ['user', 'rojo', 'crawl', 'stuber', 'thefarewell', 'bethanyhamiton']
+    try:
+        with open(filename, 'w') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+            writer.writeheader()
+            for data in list1:
+                writer.writerow(data)
+    except IOError:
+        print("I/O error") 
+
+def export_test(filename, list1):
+    csv_columns = ['user', 'headCount', 'deepmurder', 'shaft', 'hampstead', 'vault']
+    try:
+        with open(filename, 'w') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+            writer.writeheader()
+            for data in list1:
+                writer.writerow(data)
+    except IOError:
+        print("I/O error") 
+
+export_predict('./data/each_movie_TEST.csv', predict)
+export_test('./data/each_movie_PREDICT.csv', test)
+
+
+
+
+
                      
 
  
